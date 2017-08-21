b6c863cd83d8ebd0536d716a2bf76abbd89033ea
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2017-08-21 10:16:20 -0500

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,37 @@
+'use strict';
+
+//Required libraries
 const gulp = require('gulp');
 const webserver = require('gulp-webserver');
+const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass');
 
-gulp.task('server', () => {
-  gulp.src('./views')
+
+gulp.task('webserver', () => {
+  gulp.src('views')
     .pipe(webserver({
-       livereload: true,
-       host: 'localhost',
-       port: 8080,
-       open: true
+      host: "localhost",
+      port: 80,
+      livereload: true,
+      open: true
     }));
 });
+
+gulp.task('minify-css', () =>{
+  return gulp.src('./dist/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./app/css'))
+});
+
+gulp.task('sass', () => {
+  return gulp.src('./src/sass/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./src/sass/*.scss', ['sass']);
+  gulp.watch('./dist/css/*.css', ['minify-css']);
+})
+
+gulp.task('default', ['webserver', 'watch']);
